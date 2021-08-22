acb84efe90fedf0e4d083dcbcdceb9359b25999a
andreocunha <andreoliveiracunha20@gmail.com>
2021-08-21 23:24:24 -0300

    Adicao de novas linguagens

--- src/lib/helpers/highlight.ts
+++ src/lib/helpers/highlight.ts
@@ -4,13 +4,14 @@ Hljs.registerLanguage('css', require('highlight.js/lib/languages/css'))
 Hljs.registerLanguage('json', require('highlight.js/lib/languages/json'))
 Hljs.registerLanguage('less', require('highlight.js/lib/languages/less'))
 Hljs.registerLanguage('scss', require('highlight.js/lib/languages/scss'))
-Hljs.registerLanguage(
-  'javascript',
-  require('highlight.js/lib/languages/javascript')
-)
-Hljs.registerLanguage(
-  'typescript',
-  require('highlight.js/lib/languages/typescript')
-)
+Hljs.registerLanguage('javascript',require('highlight.js/lib/languages/javascript'))
+Hljs.registerLanguage('typescript',require('highlight.js/lib/languages/typescript'))
+Hljs.registerLanguage('html',require('highlight.js/lib/languages/xml'))
+Hljs.registerLanguage('java',require('highlight.js/lib/languages/java'))
+Hljs.registerLanguage('arduino',require('highlight.js/lib/languages/arduino'))
+Hljs.registerLanguage('cpp',require('highlight.js/lib/languages/cpp'))
+Hljs.registerLanguage('php',require('highlight.js/lib/languages/php'))
+Hljs.registerLanguage('python',require('highlight.js/lib/languages/python'))
+Hljs.registerLanguage('ruby',require('highlight.js/lib/languages/ruby'))
 
 export default Hljs
